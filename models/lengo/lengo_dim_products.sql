2bd92f8db5befabccdb1cbf1b29a404ddddb19c1
Clément Msika <clementmsika@MacBook-Air-de-Clement.local>
2024-08-16 15:08:45 +0200

    added lengo dim products file

---
+++ models/lengo/lengo_dim_products.sql
@@ -0,0 +1,44 @@
+{{
+config(
+  materialized='table',
+)
+}}
+
+
+WITH all_products AS (
+
+SELECT
+
+DISTINCT
+
+    product_name,
+
+    product_format,
+
+    format_family,
+
+    brand_id
+
+FROM data-finance-staging.dbt_clement_msika.lengo_presence a
+
+INNER JOIN {{ref('lengo_dim_brands')}} b ON b.brand_name = a.brand_name
+
+AND b.category = a.category
+
+AND b.sub_category = a.sub_category
+
+)
+
+SELECT
+
+    ROW_NUMBER() OVER (ORDER BY product_name, product_format, format_family, brand_id) AS product_id,
+
+    product_name,
+
+    product_format,
+
+    format_family,
+
+    brand_id
+
+FROM all_products
