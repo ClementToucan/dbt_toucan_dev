ad36288b3563373754791e804a47395984396a65
Clément Msika <clementmsika@MacBook-Air-de-Clement.local>
2024-08-16 15:57:50 +0200

    added the migration file for lengo dim shop table

---
+++ models/lengo/lengo_dim_shops.sql
@@ -0,0 +1,44 @@
+{{ config(materialized='table')  }}
+
+
+WITH all_shops AS (
+
+SELECT
+
+DISTINCT
+
+    shop_type,
+
+    channel,
+
+    latitude,
+
+    longitude,
+
+    ymd,
+
+    DATE_TRUNC(DATE(ymd), MONTH) AS ym,
+
+    quality_class_id,
+
+    geo_location_id,
+
+
+FROM data-finance-staging.dbt_clement_msika.lengo_presence a
+
+INNER JOIN {{ref('lengo_dim_geo_locations')}} d ON d.country = a.country AND d.region = a.region AND d.neighborhood = a.neighborhood AND d.zone = a.zone
+
+INNER JOIN {{ref('lengo_dim_quality_classes')}} e ON e.quality_class = a.quality_class
+
+)
+
+SELECT
+
+    ROW_NUMBER() OVER (ORDER BY shop_type, channel, latitude, longitude, ymd, quality_class_id, geo_location_id) AS shop_id,
+
+    a.*
+    
+
+FROM all_shops a
+
+ORDER BY 1
